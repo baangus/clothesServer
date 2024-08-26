bf8faab93759acde2ab9b712477efb2207a7f3cb
nitincahar <nitincahar@gmail.com>
2024-08-26 19:35:00 +0530

    first commit

---
+++ models/items.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const ratingSchema = new mongoose.Schema({
+  rate: { type: Number, required: true },
+  count: { type: Number, required: true },
+});
+
+const productSchema = new mongoose.Schema({
+  id: { type: Number, required: true, unique: true },
+  title: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+  category: { type: String, required: true },
+  image: { type: String, required: true },
+  rating: ratingSchema,
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
